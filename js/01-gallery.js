b7e8af49b2cfe6b95f6aa0e8bb1f9180cdd3183b
AndriiSkrypka <andriskrupka25011994@icloud.com>
2022-06-17 00:43:18 +0300

    dz-7-js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+// console.log(galleryItems);
+const gal = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const newGalery = createGallery(galleryItems);
+let modalImg;
+
+gal.insertAdjacentHTML("beforeend", newGalery);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+gal.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  modalImg = basicLightbox.create(
+    `
+    <img src=${event.target.dataset.source} width="500" height="500">`,
+    {
+      onShow: () => {
+        window.addEventListener("keydown", closeEsc);
+      },
+
+      onClose: () => {
+        window.removeEventListener("keydown", closeEsc);
+      },
+    }
+  );
+  modalImg.show();
+});
+
+function closeEsc(event) {
+  if (event.code !== "Escape") {
+    return;
+  }
+  modalImg.close();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gal = document.querySelector(".gallery");
+
+const newGalery = createGallery(galleryItems);
+
+gal.insertAdjacentHTML("beforeend", newGalery);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+  scrollZoom: false,
+  disableRightClick: true,
+});
